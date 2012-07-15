0c07f30fa7cfe4021b66abbb9e591a1e386dc70f
Kevin Xu <kevin@imkevinxu.com>
2012-07-14 22:01:07 -0700

    a little styling form in reg1

--- lightning_app/appurls.py
+++ lightning_app/appurls.py
@@ -4,13 +4,15 @@ from django.contrib.auth.views import login, logout
 urlpatterns = patterns('lightning_app.views',
     
     url(r'^$', 'index', name='index'),
+    url(r'^search/$', 'search', name='search'),
+    
     url(r'^login/$', login, kwargs=dict(template_name='login.html'),
         name='login'),
     url(r'^logout/$', logout, kwargs=dict(next_page='/'),
         name='logout'),
-    url(r'^search/$', 'search', name='search'),
+
     url(r'^reg1/$', 'reg1', name='reg1'),
     url(r'^reg2/$', 'reg2', name='reg2'),
     url(r'^reg3/$', 'reg3', name='reg3'),
-    
+
 )
