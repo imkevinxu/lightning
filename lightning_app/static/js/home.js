9afef95580b36ef0770bc82ad0198dfb44d6ef2f
Wylie Conlon <wylieconlon@gmail.com>
2012-07-15 09:55:55 -0700

    Cleaning up autocomplete

--- lightning_app/static/js/home.js
+++ lightning_app/static/js/home.js
@@ -3,18 +3,21 @@ $(function() {
 	var photo_thumb = $('#photo-thumb-template').html(),
 		template = Handlebars.compile(photo_thumb);
 
+	console.log(photo_thumb);
+
 	$('#find-photographer').click(function() {
-		alert('clicked');
 		$.get('/users', function(data) {
 			$('#gallery').empty();
 
 			for (var i=0, j=data.length; i<j; i++) {
 				console.log(data[i]);
 
+				var item = data[i];
+
 				var output = template({
-					username: data.username,
-					fullname: data.fullname,
-					image_url: data.pics[0].image_url
+					'username': item.username,
+					'fullname': item.fullname,
+					'image_url': item.pics[0].image_url
 				});
 
 				$('#gallery').append(output);
