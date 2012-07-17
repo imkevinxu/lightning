9d8fa4279a376e8e3a7e6371be98bbbe7c04766a
Kevin Xu <kevin@imkevinxu.com>
2012-07-17 13:34:57 -0700

    fixed registration/login issue

--- lightning_app/models.py
+++ lightning_app/models.py
@@ -45,13 +45,12 @@ class UserProfile(Base):
     def createUser(user, email=None, password=None, tag=None):
         username = user['username']
 
-        baseUser = User.objects.get_or_create(username=username) #getting a tuple
-        if baseUser[1] is 1: #if new
-            baseUser[0].email = email #needs email field
-            baseUser[0].password = password #needs password field
-        baseUser[0].save() #saves object
+        try:
+            baseUser = User.objects.create_user(username=username, email=email, password=password)
+        except:
+            baseUser = User.objects.get(username=username)
         
-        photographer = UserProfile.objects.get_or_create(user=baseUser[0])[0]
+        photographer = UserProfile.objects.get_or_create(user=baseUser)[0]
         photographer.is_photographer = True
         photographer.fhp_id = user['id']
         photographer.firstname = user['firstname']
