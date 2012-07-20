d806d94deacf99303e7da2dc3d4e8858109ffb66
Kevin Xu <kevin@imkevinxu.com>
2012-07-19 17:58:45 -0700

    added loading gif after search

--- lightning_app/static/js/home.js
+++ lightning_app/static/js/home.js
@@ -3,6 +3,7 @@ $(function() {
 	$('#tags').on("change", function() {
 		var search_tag = $('#tags').val();
 		$('#gallery').css("opacity", 0.5);
+		$('#gallery h2').first().append('<img src="http://s3.amazonaws.com/lightning-app/images/loader.gif" id="loader">');
 
 		$.get('/users/'+search_tag, function(data) {
 			var results = $('<div></div>');
@@ -19,7 +20,7 @@ $(function() {
 					    	.append($('<div></div>').addClass("name").html(item.fullname)
 					    		.append($('<span></span>').addClass("right").css("padding-right", "10px").css("margin-top", "-4px")
 					    			.append($('<span></span>').addClass("glyph general").css("font-size", "14px").css("color", "#D41616").html("b").after(affection))))
-					    	.append($('<img></img>').attr("src", item.pics[0].image_url))
+					    	.append($('<img></img>').attr("src", item.pics[0].image_url).addClass("photo"))
 					   	);
 
 				results.append(elem);
@@ -30,10 +31,10 @@ $(function() {
 
 			$('#featured_photog').append('<h2>Search for "'+search_tag+'"</h2>');
 			$('#featured_photog').append(results);
-			console.log(results);
 			$('#gallery').css("opacity", 1);
 		});
 
 		return false;
 	});
 });
+
