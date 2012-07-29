1528b055b7659a2de467e06d7f29064366edb080
Wylie Conlon <wylieconlon@gmail.com>
2012-07-29 01:19:48 -0700

    Rename reg2 to account

--- lightning_app/appurls.py
+++ lightning_app/appurls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('lightning_app.views',
     url(r'^login_redirect/$', 'login_redirect', name='login_redirect'),
 
     url(r'^register/$', 'register', name='register'),
-    url(r'^reg2/$', 'reg2', name='account'),
+    url(r'^account/$', 'account', name='account'),
     url(r'^reg3/$', 'reg3', name='reg3'),
 
     url(r'^users/(.*)/$', 'users'),
